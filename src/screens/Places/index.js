efea697de50f5e8d706ebf3a34a639f2714be069
Pulkit Kakkar <pulkitkkr@gmail.com>
2018-06-15 06:59:00 +0530

    Places Refactor

--- src/screens/Places/index.js
+++ src/screens/Places/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { GooglePlacesKey } from '../../common/apiKey';
 
 class Places extends Component {
     constructor(props) {
@@ -17,10 +19,23 @@ class Places extends Component {
             }
         }
         console.log(this.state);
-
+    }
+    componentDidMount() {
+        const config = {
+            headers: {'Access-Control-Allow-Origin': '*'}
+        };
+        const {lat, long } = this.state;
+        if(this.state.placeId === null) {
+            window.location.href = '/';
+        } else {
+            axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=5000&types=restaurant&key=${GooglePlacesKey}`, config)
+                .then(resp => {
+                console.log(resp);
+            });
+        }
     }
     render() {
-        const { lat, long, address, placeId} = this.props;
+        const { lat, long, address, placeId} = this.state;
         return(
             <h1>NearBy Places Zone {address}</h1>
         )
